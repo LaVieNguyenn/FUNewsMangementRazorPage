9092dec2c8b8d11443b6db3c4c6c959e5229e164
Thien817 <thienndse173057@fpt.edu.vn>
2025-03-05 14:57:17 +0700

    Add files via upload

---
+++ Team_07_PRN222_A02/Controllers/ReportController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Team_07_PRN222_A02.BLL.DTOs;
+using Team_07_PRN222_A02.BLL.Services;
+
+namespace Team_07_PRN222_A02.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GenerateReport(DateTime startDate, DateTime endDate)
+        {
+            var report = await _reportService.GetReportByPeriodAsync(startDate, endDate);
+            return View("ReportResult", report);
+        }
+    }
+}
---
+++ Team_07_PRN222_A02/Program.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Team_07_PRN222_A02.BLL.Mapping;
+using Team_07_PRN222_A02.BLL.Services;
+using Team_07_PRN222_A02.BLL.Services.CategoryService;
+using Team_07_PRN222_A02.BLL.Services.NewsArticleService;
+using Team_07_PRN222_A02.BLL.Services.SystemAccountService;
+using Team_07_PRN222_A02.DAL.Models;
+using Team_07_PRN222_A02.DAL.Repositories.AccountRepository;
+using Team_07_PRN222_A02.DAL.Repositories.CategoryRepository;
+using Team_07_PRN222_A02.DAL.Repositories.NewsRepository;
+using Team_07_PRN222_A02.DAL.UnitOfWork;
+
+namespace Team_07_PRN222_A02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<FunewsManagementContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionStringDB")));
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddScoped<IReportService, ReportService>();
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Authentication/Login";
+        options.AccessDeniedPath = "/";
+    });
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("StaffOnly", policy => policy.RequireRole("Staff"));
+                options.AddPolicy("AdminOrStaff", policy => policy.RequireRole("Admin", "Staff"));
+            });
+            builder.Services.AddHttpContextAccessor();
+
+            builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+            builder.Services.AddScoped<ISystemAccountService, SystemAccountService>();
+
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+            builder.Services.AddScoped<INewsRepository, NewRepository>();
+            builder.Services.AddScoped<INewArticleService, NewArticleService>();
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
