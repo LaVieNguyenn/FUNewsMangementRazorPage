db572a37a44b5103cdfef17f7bfbc9718dcb92fa
Thien817 <thienndse173057@fpt.edu.vn>
2025-03-10 23:23:38 +0700

    Add files via upload

---
+++ Team_07_PRN222_A02.BLL/DTOs/NewsArticleUpdateDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_07_PRN222_A02.BLL.DTOs
+{
+    public class NewsArticleUpdateDTO
+    {
+        public int Id { get; set; }
+        public int? NewsArticleId { get; set; }
+
+        public string? NewsTitle { get; set; } = null!;
+
+        public string? Headline { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public string NewsContent { get; set; } = null!;
+
+        public string? NewsSource { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public string? NewsStatus { get; set; } = null!;
+
+        public int CreatedById { get; set; }
+
+        public int? UpdatedById { get; set; }
+    }
+}
---
+++ Team_07_PRN222_A02.BLL/Services/Reports/IReportService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Team_07_PRN222_A02.BLL.DTOs;
+
+namespace Team_07_PRN222_A02.BLL.Services
+{
+    public interface IReportService
+    {
+        Task<List<NewsArticleDTO>> GetReportByPeriodAsync(DateTime startDate, DateTime endDate);
+    }
+}
