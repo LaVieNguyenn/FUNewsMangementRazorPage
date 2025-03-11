a6fbe0c7343f8970ebcdf9989fbec78c3b092da6
Thien817 <thienndse173057@fpt.edu.vn>
2025-03-11 23:38:24 +0700

    Add files via upload

---
+++ Team_07_PRN222_A02.DAL/Repositories/AccountRepository/AccountRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Team_07_PRN222_A02.DAL.Models;
+
+namespace Team_07_PRN222_A02.DAL.Repositories.AccountRepository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly FunewsManagementContext _context;
+
+        public AccountRepository(FunewsManagementContext funewsManagementContext)
+        {
+            _context = funewsManagementContext;
+        }
+        public async Task DeleteAsync(SystemAccount obj) => _context.SystemAccounts.Remove(obj);
+
+        public IQueryable<SystemAccount> GetAllAsync()
+        {
+            return _context.SystemAccounts.AsQueryable();
+        }
+
+        public async Task<SystemAccount?> GetByIdAsync(int obj) => await _context.SystemAccounts.FirstOrDefaultAsync(a => a.AccountId == obj);
+
+        public async Task InsertAsync(SystemAccount obj) => await _context.SystemAccounts.AddAsync(obj);
+
+        public async Task<SystemAccount?> LoginAsync(string username, string password) => await _context.SystemAccounts.FirstOrDefaultAsync(a => a.AccountEmail == username && a.AccountPassword == password);
+
+        public async Task<int> SaveChangesAsync() => await _context.SaveChangesAsync();
+
+        public async Task UpdateAsync(SystemAccount obj) => _context.SystemAccounts.Update(obj);
+        public async Task UpdateAccount(SystemAccount obj)                                                                          
+        {
+            _context.SystemAccounts.Update(obj);
+            await _context.SaveChangesAsync(); 
+        }
+        public async Task<SystemAccount?> GetByEmailAsync(string email)
+        {
+            return await _context.SystemAccounts
+                                 .FirstOrDefaultAsync(a => a.AccountEmail == email);
+        }
+
+
+        public async Task<SystemAccount?> GetAccountByEmailAsync(string email)
+        {
+            return await _context.SystemAccounts
+                .FirstOrDefaultAsync(a => a.AccountEmail == email);
+        }
+
+        public async Task<SystemAccount> GetAccountById(int accountID)
+        {
+            return await _context.SystemAccounts.FindAsync(accountID);
+        }
+
+        public async Task UpdateAccountAsync(SystemAccount account)
+        {
+            _context.SystemAccounts.Update(account);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ Team_07_PRN222_A02.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,94 @@
+﻿using Team_07_PRN222_A02.DAL.Models;
+using Team_07_PRN222_A02.DAL.Repositories.AccountRepository;
+using Team_07_PRN222_A02.DAL.Repositories.CategoryRepository;
+using Team_07_PRN222_A02.DAL.Repositories.NewsRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Team_07_PRN222_A02.DAL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly FunewsManagementContext _context;
+
+        private IAccountRepository? _accountRepository;
+        private INewsRepository? _newArticleRepository;
+        private ICategoryRepository? _categoryRepository;
+        private bool disposed = false;
+        public UnitOfWork(FunewsManagementContext context)
+        {
+            _context = context;
+        }
+        public IAccountRepository AccountRepository
+        {
+            get
+            {
+                if (_accountRepository == null)
+                {
+                    _accountRepository = new AccountRepository(_context);
+                }
+                return _accountRepository;
+            }
+        }
+        public INewsRepository NewsArticleRepository
+        {
+            get
+            {
+                if (_newArticleRepository == null)
+                {
+                    _newArticleRepository = new NewRepository(_context);
+                }
+                return _newArticleRepository;
+            }
+        }
+        public ICategoryRepository CategoryRepository
+        {
+            get
+            {
+                if (_categoryRepository == null)
+                {
+                    _categoryRepository = new CategoryRepository(_context);
+                }
+                return _categoryRepository;
+            }
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public void BeginTransaction()
+        {
+            _context.Database.BeginTransaction();
+        }
+
+        public void CommitTransaction()
+        {
+            _context.Database.CommitTransaction();
+        }
+
+        public void RollBack()
+        {
+            _context.Database.RollbackTransaction();
+        }
+    }
+}
