5ac97576f9d415581ba0fc532f9bcf4e4e11c1a1
Thien817 <thienndse173057@fpt.edu.vn>
2025-03-14 20:07:25 +0700

    Update NewsArticleUpdateDTO.cs

--- Team_07_PRN222_A02.BLL/DTOs/NewsArticleUpdateDTO.cs
+++ Team_07_PRN222_A02.BLL/DTOs/NewsArticleUpdateDTO.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Team_07_PRN222_A02.BLL.DTOs
 {
@@ -10,23 +6,14 @@ namespace Team_07_PRN222_A02.BLL.DTOs
     {
         public int Id { get; set; }
         public int? NewsArticleId { get; set; }
-
-        public string? NewsTitle { get; set; } = null!;
-
+        public string? NewsTitle { get; set; }
         public string? Headline { get; set; }
-
         public DateTime? CreatedDate { get; set; }
-
         public string NewsContent { get; set; } = null!;
-
         public string? NewsSource { get; set; }
-
         public int? CategoryId { get; set; }
-
-        public string? NewsStatus { get; set; } = null!;
-
+        public byte? NewsStatus { get; set; } 
         public int CreatedById { get; set; }
-
         public int? UpdatedById { get; set; }
     }
 }
