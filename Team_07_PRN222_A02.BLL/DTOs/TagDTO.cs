dc259c383212d377409590dd21d18c05a2c67e8a
Thien817 <thienndse173057@fpt.edu.vn>
2025-03-19 00:38:00 +0700

    Add files via upload

---
+++ Team_07_PRN222_A02.BLL/DTOs/TagDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_07_PRN222_A02.BLL.DTOs
+{
+    public class TagDTO
+    {
+        public int TagId { get; set; }
+        public string TagName { get; set; }
+    }
+}
