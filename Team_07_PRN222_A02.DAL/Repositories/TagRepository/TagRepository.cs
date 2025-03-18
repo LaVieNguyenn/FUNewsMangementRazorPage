80652a537c95405540d6837bcf831493cf7dc9b1
Thien817 <thienndse173057@fpt.edu.vn>
2025-03-19 00:39:51 +0700

    Add files via upload

---
+++ Team_07_PRN222_A02.DAL/Repositories/TagRepository/TagRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Team_07_PRN222_A02.DAL.Models;
+
+namespace Team_07_PRN222_A02.DAL.Repositories.TagRepository
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly FunewsManagementContext _context;
+
+        public TagRepository(FunewsManagementContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Tag>> GetAllTagsAsync()
+        {
+            return await _context.Tags.ToListAsync();
+        }
+    }
+}
